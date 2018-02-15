6cee06a7b70a90d7b8b7a98fb9a7f7dbc77d3de2
Adam Weidner <aweidner6993@gmail.com>
2018-02-15 17:57:56 -0500

    Adds the beginnings of a parser library

--- spec/create_table_parser.lua
+++ spec/create_table_parser.lua
@@ -1,4 +1,9 @@
 local match_pattern = require("parser").match_pattern
+local pattern = require("parser").pattern
+local anyOf = require("parser").anyOf
+local compose = require("parser").compose
+local types = require("parser").types
+local column_def = require("parser").column_def
 
 describe("Match Pattern", function()
     it("Should be able to parse the text create table", function()
@@ -17,5 +22,64 @@ describe("Match Pattern", function()
         local result = match_pattern("abcdefg hello", "[^ ]*")
         assert.equals(result.success, true)
         assert.equals(result.rem, " hello")
+    end)
+end)
+
+describe("pattern", function()
+    it("Should be able to match one pattern and give output", function()
+        local result = pattern("[a-e]+")("abcdefgh")
+        assert.equals(result.matched, "abcde")
+        assert.equals(result.success, true)
+    end)
+end)
+
+describe("anyOf", function()
+    it("Should be able to match any of a set of pattern matchers", function()
+        local result = anyOf({pattern("[a-e]"), pattern("[z]+")})("zzzz")
+        assert.equals(result.matched, "zzzz")
+    end)
+
+    it("Should return false and the original text if it could not match", function()
+        local result = anyOf({pattern("[a-e]"), pattern("[z]+")})("yyyy")
+        assert.equals(result.matched, "")
+        assert.equals(result.rem, "yyyy")
+    end)
+end)
+
+describe("compose", function()
+    it("Should be able to combine multiple matchers", function()
+        local result = compose({
+            pattern("[a-e]+"), pattern("%s+"), pattern("zzz")})("aaa zzzbbbb")
+
+        assert.equals(result.matched, "aaa zzz")
+        assert.equals(result.rem, "bbbb")
+    end)
+
+    it("Should return as much as it could match", function()
+        local result = compose({pattern("aaa"), pattern("zzz")})("aaa  zzz")
+        assert.equals(result.rem, "  zzz")
+        assert.equals(result.success, false)
+    end)
+end)
+
+describe("types and column def", function()
+    it("Should be able to parse a type", function()
+        local result = types("int")
+        assert.equals(result.matched, "int")
+
+        result = types("string")
+        assert.equals(result.matched, "string")
+
+        result = types("does not exist")
+        assert.equals(result.success, false)
+    end)
+
+    it("Should parse a column definition", function()
+        local result = column_def("hello_world int")
+        assert.equals(result.matched, "hello_world int")
+
+        result = column_def("hello_world nonsense_type")
+        assert.equals(result.matched, "hello_world ")
+        assert.equals(result.success, false)
     end)
 end)
--- src/parser.lua
+++ src/parser.lua
@@ -1,16 +1,61 @@
 local parser = {}
 
-local function result(success, remaining)
-    return {success = success, rem = remaining}
+local function result(success, remaining, matched)
+    return {success = success, rem = remaining, matched = matched}
 end
 
 function parser.match_pattern(text, look_for)
     local matched = text:match(look_for)
 
     if matched then
-        return result(true, text:sub(#matched + 1, #text))
+        return result(true, text:sub(#matched + 1, #text), matched)
     end
-    return result(false, text)
+    return result(false, text, "")
 end
+
+function parser.pattern(pattern)
+    return function(text)
+        return parser.match_pattern(text, "^" .. pattern)
+    end
+end
+
+function parser.anyOf(matchers)
+    return function(text)
+        for _, matcher in pairs(matchers) do
+            local match_results = matcher(text)
+            if match_results.success then
+                return match_results
+            end
+        end
+
+        return result(false, text, "")
+    end
+end
+
+function parser.compose(matchers)
+    return function(text)
+        local remaining = text
+        local matched = ""
+
+        for _, matcher in pairs(matchers) do
+            local match_results = matcher(remaining)
+
+            if not match_results.success then
+                return result(false, remaining, matched)
+            end
+
+            matched = matched .. match_results.matched
+            remaining = match_results.rem
+        end
+
+        return result(true, remaining, matched)
+    end
+end
+
+
+parser.types = parser.anyOf({parser.pattern("int"), parser.pattern("string")})
+parser.column_def = parser.compose({
+    parser.pattern("[a-zA-Z_]+"),
+    parser.pattern("%s+"), parser.types})
 
 return parser
